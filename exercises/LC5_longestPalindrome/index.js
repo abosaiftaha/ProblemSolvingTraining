d699cdf62e16b52db40052d94ea3df65b5315d36
Ahmad Taha <ataha@netways.com>
2021-06-07 01:19:02 +0300

    💡 5. brute-force solution

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,31 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+// 💭 Suggested Solutions
+// 1- loop over each substring & check is it's palindrome then return the longest palindrome ===> O(n^2)
+// 2- Expand on left & right of each letter ==> O(n)
+
+// function longestPalindrome(s) {
+// }
+
+//! Gave TLE
+function longestPalindrome(s) {
+    s = `${s}`;
+    let arr= [];
+    let result = "";
+    
+    for(let i = 0; i < s.length; i++){
+        for(let j = 0; j < s.length; j++){
+            const subStringValue = s.slice(i,j+1); 
+            arr.push(subStringValue);
+        }
+    }
+
+    for(let element of arr){
+        if(element === element.split("").reverse().join("") && element.length > result.length) result = element;
+    }
+
+    return result;
+}
 
 module.exports = longestPalindrome;
