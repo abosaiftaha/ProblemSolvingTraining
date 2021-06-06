1d1bd86d355e1cbb226c937ab17fd4be9fa3c0a2
Ahmad Taha <ataha@netways.com>
2021-06-07 00:22:32 +0300

    ✨ 125. final solution

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -6,10 +6,25 @@
 // 2. 2 pointers & check single char each time ==> O(n);
 
 
+// function isPalindrome(s) {
+//     const finalString = s.replace(/[\W_]/g, '').toLowerCase();
+
+//     return finalString === finalString.split("").reverse().join("");
+// }
+
 function isPalindrome(s) {
     const finalString = s.replace(/[\W_]/g, '').toLowerCase();
+    let leftPointer = 0;
+    let rightPointer = finalString.length - 1;
 
-    return finalString === finalString.split("").reverse().join("");
+    console.log(leftPointer < rightPointer, finalString[leftPointer], finalString[rightPointer]);
+    while (leftPointer <= rightPointer) {
+        if(finalString[leftPointer] !== finalString[rightPointer]) return false;
+        leftPointer++;
+        rightPointer--;
+    }
+
+    return true;
 }
 
 module.exports = isPalindrome;
