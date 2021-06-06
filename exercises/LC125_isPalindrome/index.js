d170e22176715cded54481e3de58b3017576f953
Ahmad Taha <ataha@netways.com>
2021-06-07 00:21:52 +0300

    💡 125. brute-force solution

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,15 @@
-function isPalindrome(s) {}
+// s = "A man, a plan, a canal: Panama" ===> return true
+// s = "race a car" ===> return false
+
+// My Suggested Solutions:
+// 1. use reverse function ==> O(n)
+// 2. 2 pointers & check single char each time ==> O(n);
+
+
+function isPalindrome(s) {
+    const finalString = s.replace(/[\W_]/g, '').toLowerCase();
+
+    return finalString === finalString.split("").reverse().join("");
+}
 
 module.exports = isPalindrome;
